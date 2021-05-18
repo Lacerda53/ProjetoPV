253303ab01868c4076fd2f106957db2cdb33565b
Guilherme Andrade <guilherme-andrade1110@hotmail.com>
2021-05-18 01:51:44 -0300

    first commit

---
+++ src/components/ButtonMain.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  ActivityIndicator
+} from "react-native";
+import colors from "../styles/colors";
+
+interface IButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+export function ButtonMain({ title, disabled = false, ...rest }: IButtonProps) {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest}>
+      {disabled ? <ActivityIndicator color={colors.white} size="large" /> : <Text style={styles.text}>{title.toUpperCase()}</Text>}
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    height: 56,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 16,
+  },
+});
---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export default {
+    primary: '#4285F4',
+    title: '#1C4C9A',
+    labelForm: '#434343',
+    textForm: '#9D9D9D',
+    shape: '#EEEEEE',
+    white: "#FFF",
+    gray: '#CFCFCF',
+    base: '#757575',
+    red: '#e83f5b'
+};
